4fc00e666b2aa82e8d7f3aff459e5f0449b7cc96
Craftless <javier.tzh@gmail.com>
2022-04-10 09:08:54 +0800

    changed fetch to async await

--- src/pages/AllMeetups.tsx
+++ src/pages/AllMeetups.tsx
@@ -4,30 +4,42 @@ import { useEffect, useState } from "react";
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [loadedMeetups, setLoadedMeetups] = useState<Meetup[]>();
 
-  useEffect(() => {
+  const fetchMeetups = async () => {
     setIsLoading(true);
-    fetch(
-      "https://react-meetup-course-a72de-default-rtdb.firebaseio.com/meetups.json"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const meetups: Meetup[] = [];
+    setError(null);
 
-        for (const key in data) {
-          const meetup: Meetup = {
-            id: key,
-            ...data[key],
-          };
-          meetups.push(meetup);
-        }
+    try {
+      const response = await fetch(
+        "https://react-meetup-course-a72de-default-rtdb.firebaseio.com/meetups.json"
+      );
 
-        setIsLoading(false);
-        setLoadedMeetups(meetups);
-      });
+      if (!response.ok) throw new Error("Request Failed!");
+
+      const data = await response.json();
+
+      const meetups: Meetup[] = [];
+
+      for (const key in data) {
+        const meetup: Meetup = {
+          id: key,
+          ...data[key],
+        };
+        console.log(key);
+        meetups.push(meetup);
+      }
+      setLoadedMeetups(meetups);
+    } catch (error: any) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMeetups();
   }, []);
 
   if (isLoading) {
@@ -37,11 +49,29 @@ function AllMeetupsPage() {
       </section>
     );
   }
+  if (error) {
+    return (
+      <section>
+        <p>Something went wrong.</p>
+        <button
+          onClick={() => {
+            fetchMeetups();
+          }}
+        >
+          Try again.
+        </button>
+      </section>
+    );
+  }
 
   return (
     <section>
       <h1>All meetups page</h1>
-      { loadedMeetups !== (null || undefined) ? <MeetupList meetups={loadedMeetups as Meetup[]} /> : <p>You have no meetups. Try adding a new meetup.</p>}
+      {loadedMeetups !== (null || undefined) ? (
+        <MeetupList meetups={loadedMeetups as Meetup[]} />
+      ) : (
+        <p>You have no meetups. Try adding a new meetup.</p>
+      )}
     </section>
   );
 }
